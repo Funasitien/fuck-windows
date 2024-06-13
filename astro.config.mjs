d7ca69a49dd343c4a74dfcf9830d79e1ebfcd417
Funasitien DreamCloud <funasitien@dreamclouds.fr>
2024-06-13 13:09:05 +0200

    📝 Add domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://f.dreamclouds.fr";
+const LIVE_URL = "https://linux.dreamc.site";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -31,7 +31,6 @@ export default defineConfig({
   server: {
     port: SERVER_PORT
   },
-  base: 'fuck-windows',
   site: BASE_URL,
   integrations: [sitemap(), tailwind({
     config: {
